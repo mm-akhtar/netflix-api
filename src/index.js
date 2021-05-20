9299dd347886864fbc09dfc403945685c1198256
Md Masrur Akhtar <kkakhtar143@gmail.com>
2021-05-20 17:57:20 +0530

    CORS add

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ app.use(express.json());
 app.use(userRouter);
 app.use(favRouter);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`server is up on ${port}`);
